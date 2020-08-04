1385a199cbc76f99bf749a0248bee17e4d5221a8
mich <mich-cook@users.noreply.github.com>
2020-08-03 23:28:36 -0700

    handle successful feedback submissions

--- 2255009-nodejs-express/routes/feedback.js
+++ 2255009-nodejs-express/routes/feedback.js
@@ -4,6 +4,13 @@ const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 
+const validations = [
+  check('name').trim().isLength({ "min": 3}).escape().withMessage(`Name is not valid.`),
+  check('email').trim().isEmail().normalizeEmail().withMessage(`Email is not valid`),
+  check('title').trim().isLength({ "min": 3}).escape().withMessage(`Title is not valid`),
+  check('message').trim().isLength({ "min": 5}).escape().withMessage(`Message is not valid`)
+];
+
 module.exports = args => {
 
   const { feedbackService } = args;
@@ -12,20 +19,16 @@ module.exports = args => {
     try {
       const feedback = await feedbackService.getList();
       const errors = req.session.feedback ? req.session.feedback.errors : false;
+      const successMessage = req.session.feedback ? req.session.feedback.message : false;
       req.session.feedback = {};
       // template-specific vars
-      return res.render('layout', { "title": "Feedback", "template": "feedback", feedback, errors });
+      return res.render('layout', { "title": "Feedback", "template": "feedback", feedback, errors, successMessage });
     } catch(err) {
       return next(err);
     }
   });
 
-  router.post('/', [
-      check('name').trim().isLength({ "min": 3}).escape().withMessage(`Name is not valid.`),
-      check('email').trim().isEmail().normalizeEmail().withMessage(`Email is not valid`),
-      check('title').trim().isLength({ "min": 3}).escape().withMessage(`Title is not valid`),
-      check('message').trim().isLength({ "min": 5}).escape().withMessage(`Message is not valid`)],
-    (req, res) => {
+  router.post('/', validations, async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       req.session.feedback = {
@@ -34,7 +37,14 @@ module.exports = args => {
       return res.redirect('/feedback');
     }
 
-    return res.send(`Post handler for feedback form`);
+    const { name, email, title, message } = req.body;
+    await feedbackService.addEntry(name, email, title, message);
+
+    req.session.feedback = {
+      "message": "Your message has been received successfully."
+    };
+
+    return res.redirect('/feedback');
   });
 
   return router;
