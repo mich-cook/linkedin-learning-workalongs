7a70ba156c3ad9052c2097a4fccf317fa2c48a62
mich <mich-cook@users.noreply.github.com>
2020-08-03 18:35:50 -0700

    set up session cookies to use in the near future

--- 2255009-nodejs-express/server.js
+++ 2255009-nodejs-express/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
 
 const FeedbackService = require('./tainted-code-to-remove-or-implement/services/FeedbackService');
 const SpeakersService = require('./tainted-code-to-remove-or-implement/services/SpeakerService');
-
 const feedbackService = new FeedbackService('./tainted-code-to-remove-or-implement/data/feedback.json');
 const speakersService = new SpeakersService('./tainted-code-to-remove-or-implement/data/speakers.json');
 
@@ -11,6 +11,13 @@ const routes = require('./routes');
 
 const app = express();
 const port = 3000;
+
+app.set('trust proxy', 1); // cookies through reverse proxy are ok.
+
+app.use(cookieSession({
+    name: 'session',
+    keys: ['hdhfhwjefkvksk3kk88d8cc', 'fh2h329f9fhn23r0fbfaal3ly']  // just random
+}));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
