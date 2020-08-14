5f041972df66a706253847e339456d42cc37cd43
mich <mich-cook@users.noreply.github.com>
2020-08-14 02:00:22 -0700

    add header/footer to page (content TDB) update the content in App.js from default to project stuff

--- 808689-react-interface/src/components/App.js
+++ 808689-react-interface/src/components/App.js
@@ -3,20 +3,10 @@ import '../css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="petratings">
+      <button>Add Appointments</button>
+      <button>Search Appointments</button>
+      <button>List Appointments</button>
     </div>
   );
 }
