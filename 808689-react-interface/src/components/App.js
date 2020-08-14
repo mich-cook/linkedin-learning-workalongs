d57cf7e1d46066e6bbc28a4d4c7d98afc1e6f7ca
mich <mich-cook@users.noreply.github.com>
2020-08-14 15:43:18 -0700

    github CORS blocks access to data in a gist

--- 808689-react-interface/src/components/App.js
+++ 808689-react-interface/src/components/App.js
@@ -15,11 +15,10 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://gist.github.com/mich-cook/dbae71204c97019dda3d066b11114a5d')
+    fetch('./tmpdata-appointments.json')
       .then(response => response.json())
       .then(result => {
-        const appointments = result.map(item => { return item; });
-        this.setState({ appointments });
+        this.setState({ "appointments": result });
       });
   }
 
