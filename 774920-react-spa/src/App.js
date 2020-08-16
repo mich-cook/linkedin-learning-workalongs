bc4e9ab843551aff01130de3171d6183f4569cc1
mich <mich-cook@users.noreply.github.com>
2020-08-16 01:41:27 -0700

    update Home component to use user that's passed as a prop from the App's state and display buttons accordingly

--- 774920-react-spa/src/App.js
+++ 774920-react-spa/src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 import Home from './Home.js';
 
 export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      "user": null
+    };
+  }
+
   render() {
-    return <Home />;
+    return (
+      <div>
+        <Home userName={this.state.user} />
+      </div>
+    );
   }
 }
--- 774920-react-spa/src/Home.js
+++ 774920-react-spa/src/Home.js
@@ -3,13 +3,20 @@ import React, { Component } from 'react';
 export default class Home extends Component {
 
   render() {
+    const uselessStyle = { "precomputedStyle": true }
+    const { userName } = this.props;
+
     return (
-      <div className="App">
-        <h1>Meeting Log</h1>
+      <div className="App" style={uselessStyle}>
+        <h1 style={{ "fontSize": 3 + "rem", "inlineStyle": true }}>Meeting Log</h1>
         <ul>
+          {userName === null && (<>
           <li><a href="/register">Register</a></li>
           <li><a href="/login">Log In</a></li>
+          </>)}
+          {userName !== null && (<>
           <li><a href="/meetings">Meetings</a></li>
+          </>)}
         </ul>
       </div>
     );
