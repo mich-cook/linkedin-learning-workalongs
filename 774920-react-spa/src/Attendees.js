30796b34467fa42e7b3706100878bd7c6b99fcde
mich <mich-cook@users.noreply.github.com>
2020-08-18 23:26:23 -0700

    clear attendee search input box and (for some reason) just get a random attendee

--- 774920-react-spa/src/Attendees.js
+++ 774920-react-spa/src/Attendees.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { FaUndo, FaRandom } from 'react-icons/fa';
 import firebase from './Firebase.js';
 import AttendeeList from './AttendeeList.js';
 
@@ -6,11 +7,14 @@ export default class Attendees extends Component {
   constructor() {
     super();
     this.state = {
+      "allAttendees": [],
       "attendeeNames": [],
       "keywords": ""
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.resetQuery = this.resetQuery.bind(this);
+    this.chooseRandom = this.chooseRandom.bind(this);
   }
 
   componentDidMount() {
@@ -28,6 +32,7 @@ export default class Attendees extends Component {
       }
 
       this.setState({
+        "allAttendees": attendeeList,
         "attendeeNames": attendeeList
       });
     });
@@ -44,6 +49,19 @@ export default class Attendees extends Component {
     e.preventDefault();
   }
 
+  resetQuery() {
+    this.setState({
+      "keywords": "",
+      "attendeeNames": this.state.allAttendees
+    });
+  }
+
+  chooseRandom() {
+    const randomAttendee = Math.floor(Math.random() * this.state.allAttendees.length);
+    this.resetQuery();
+    this.setState({ "attendeeNames": [this.state.allAttendees[randomAttendee]] });
+  }
+
   render() {
 
     const filter = item => item.attendeeName.toLowerCase().match(this.state.keywords.toLowerCase()) && true;
@@ -53,6 +71,8 @@ export default class Attendees extends Component {
       <h1>Attendees</h1>
       <form onSubmit={this.handleSubmit}>
         <input type="text" name="keywords" value={this.state.keywords} placeholder="Search Attendees" className="" onChange={this.handleChange}/>
+        <button className="input-group-append" title="Reset Search" onClick={this.resetQuery}><FaUndo /></button>
+        <button title="Random Attendee" onClick={this.chooseRandom}><FaRandom /></button>
       </form>
       <AttendeeList userID={this.props.userID} adminUser={this.props.adminUser} attendees={filteredAttendees} meetingID={this.props.meetingID} />
     </>);
